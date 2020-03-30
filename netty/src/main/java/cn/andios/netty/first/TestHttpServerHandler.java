6f4f6eca73082cfeb7f0b93d0668f82fd63e8fd8
lsd <1121840811@qq.com>
2020-03-30 12:37:51 +0800

    执行流程

--- netty/src/main/java/cn/andios/netty/first/TestHttpServerHandler.java
+++ netty/src/main/java/cn/andios/netty/first/TestHttpServerHandler.java
@@ -2,18 +2,20 @@ package cn.andios.netty.first;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.handler.codec.http.*;
 import io.netty.util.CharsetUtil;
 
-import java.nio.charset.Charset;
+import java.net.URI;
 
 /**
  * @description:
  * @author:LSD
  * @when:2020/03/29/22:04
  */
+@ChannelHandler.Sharable
 public class TestHttpServerHandler extends SimpleChannelInboundHandler<HttpObject> {
 
     /**
@@ -24,7 +26,22 @@ public class TestHttpServerHandler extends SimpleChannelInboundHandler<HttpObjec
      */
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, HttpObject msg) throws Exception {
+
+        System.out.println(msg.getClass());
+        System.out.println(ctx.channel().remoteAddress());
+
         if(msg instanceof HttpRequest){
+            System.out.println("执行channelRead0");
+
+            HttpRequest httpRequest = (HttpRequest)msg;
+            System.out.println("请求方法名：" + httpRequest.getMethod().name());
+
+            URI uri = new URI(httpRequest.uri());
+            if("/favicon.ico".equals(uri.getPath())){
+                System.out.println("请求favicon.ico");
+                return;
+            }
+
             ByteBuf content = Unpooled.copiedBuffer("hello world",CharsetUtil.UTF_8);
 
             FullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1,HttpResponseStatus.OK,content);
@@ -32,7 +49,37 @@ public class TestHttpServerHandler extends SimpleChannelInboundHandler<HttpObjec
             response.headers().set(HttpHeaderNames.CONTENT_LENGTH,content.readableBytes());
 
             ctx.writeAndFlush(response);
+            ctx.channel().close();
         }
+    }
 
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channel active");
+        super.channelActive(ctx);
+    }
+
+    @Override
+    public void channelRegistered(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channel registered");
+        super.channelRegistered(ctx);
+    }
+
+    @Override
+    public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("handler added");
+        super.handlerAdded(ctx);
+    }
+
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channel inactive");
+        super.channelInactive(ctx);
+    }
+
+    @Override
+    public void channelUnregistered(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channel unregistered");
+        super.channelUnregistered(ctx);
     }
 }
--- netty/src/main/java/cn/andios/netty/first/TestServer.java
+++ netty/src/main/java/cn/andios/netty/first/TestServer.java
@@ -26,6 +26,5 @@ public class TestServer {
             bossGroup.shutdownGracefully();
             workerGroup.shutdownGracefully();
         }
-
     }
 }
